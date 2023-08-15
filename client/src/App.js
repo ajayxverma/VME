d2ecbc637ec151066b87bd1f893266165310206a
ajayxverma <aajayaverma88927@gmail.com>
2023-08-15 18:34:34 +0530

    1834T

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
-import profilePage from "scenes/profilePage";
+import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react";
 import { UseSelector, useSelector } from "react-redux/es/hooks/useSelector";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -12,17 +12,22 @@ import { Css } from "@mui/icons-material";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-
           <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<profilePage />} />
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
